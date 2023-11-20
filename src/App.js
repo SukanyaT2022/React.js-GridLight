fc5244c1eb7b8f6498b73b68823736262a707cca
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-20 15:26:18 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import './App.css'
+import { configure } from '@testing-library/react'
+
+const App = () => {
+const [order, setOrder] = useState([])
+const [isDeactivating, setIsDeactivating] = useState(false)
+
+// variable contain 3 by 3 array
+const config = [
+  [1, 1, 1], 
+  [1, 0, 1], 
+  [1, 1, 1]
+]
+
+
+  return (
+   <>
+   <div className='wrapper'>
+   <div className='grid'>
+{config.flat(1).map((value,index)=>{
+  return value ? (
+
+  ):
+   (
+    
+   )
+
+})}
+
+
+{/* Array.flat() help to make ex const arr1 = [0, 1, 2, [3, 4]];
+array inside array combime to be one array
+expected output: Array [0, 1, 2, 3, 4, 5] */}
+{/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat */}
+   </div>
+
+   </div>
+   </>
+      
+  )
+}
+
+export default App
